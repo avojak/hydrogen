0db051d2d841ae2ffc2ccfa69dd1ffdd6f7f4fed
TheDesertMonk <vojak1@illinois.edu>
2017-06-04 16:32:37 -0500

    Adding H2 .jar for non-automated testing

---
+++ hydrogen.test/src/main/java/com/thedesertmonk/plugin/hydrogen/test/H2Tester.java
@@ -0,0 +1,47 @@
+package com.thedesertmonk.plugin.hydrogen.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * @author andrewvojak
+ *
+ */
+@SuppressWarnings("nls")
+public class H2Tester {
+
+	private static final String H2_DRIVER_CLASS = "org.h2.Driver";
+	private static final String JDBC_URL = "jdbc:h2:ssl://10.0.0.9:9092/~/test";
+	private static final String USER = "sa";
+	private static final String PASS = "";
+
+	private static final String CREATE_TABLE;
+	private static final String INSERT;
+
+	static {
+		//@formatter:off
+		CREATE_TABLE = "CREATE TABLE CUSTOMERS("
+						+ "ID   INT              NOT NULL,"
+				   		+ "NAME VARCHAR (20)     NOT NULL,"
+				   		+ "AGE  INT              NOT NULL,"
+				   		+ "PRIMARY KEY (ID)"
+				   		+ ");";
+		INSERT = "INSERT INTO CUSTOMERS (ID,NAME,AGE)"
+					+ "VALUES (1, 'Andrew', 32);";
+		//@formatter:on
+	}
+
+	/**
+	 *
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(final String... args) throws Exception {
+		Class.forName(H2_DRIVER_CLASS);
+		final Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASS);
+		connection.prepareStatement(CREATE_TABLE).execute();
+		connection.prepareStatement(INSERT).execute();
+		connection.close();
+	}
+
+}
