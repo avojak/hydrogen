3852c734e5c18b358b7284ad0481fab82f6913a3
avojak <avojak@users.noreply.github.com>
2018-08-18 16:26:40 -0600

    Remove use of POSIX file permissions (#27)
    * Remove use of POSIX file permissions which break ability to build on Windows
    
    * Fix failing test; adding assertion messages

--- com.thedesertmonk.plugin.hydrogen.test/src/main/java/com/thedesertmonk/plugin/hydrogen/test/contributions/preferencepage/FileValidatorTest.java
+++ com.thedesertmonk.plugin.hydrogen.test/src/main/java/com/thedesertmonk/plugin/hydrogen/test/contributions/preferencepage/FileValidatorTest.java
@@ -3,14 +3,10 @@ package com.thedesertmonk.plugin.hydrogen.test.contributions.preferencepage;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Collections;
-import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,12 +21,6 @@ import com.thedesertmonk.plugin.hydrogen.core.contributions.preferencepage.FileV
 @SuppressWarnings("nls")
 public class FileValidatorTest {
 
-	private static final String PREFIX = "hydrogen-junit";
-	private static final FileAttribute<Set<PosixFilePermission>> OWNER_READ_ATTRIBUTE = PosixFilePermissions
-			.asFileAttribute(Collections.singleton(PosixFilePermission.OWNER_READ));
-	private static final FileAttribute<Set<PosixFilePermission>> OWNER_EXECUTE_ATTRIBUTE = PosixFilePermissions
-			.asFileAttribute(Collections.singleton(PosixFilePermission.OWNER_EXECUTE));
-
 	private FileValidator validator;
 
 	/**
@@ -42,8 +32,8 @@ public class FileValidatorTest {
 	}
 
 	/**
-	 * Tests that {@link FileValidator#isDirectory(Path)} throws an exception
-	 * when the given {@link Path} is {@code null}.
+	 * Tests that {@link FileValidator#isDirectory(Path)} throws an exception when
+	 * the given {@link Path} is {@code null}.
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testIsDirectory_NullPath() {
@@ -51,36 +41,34 @@ public class FileValidatorTest {
 	}
 
 	/**
-	 * Tests that {@link FileValidator#isDirectory(Path)} returns {@code false}
-	 * when the given {@link Path} refers to a file which is not a directory.
+	 * Tests that {@link FileValidator#isDirectory(Path)} returns {@code false} when
+	 * the given {@link Path} refers to a file which is not a directory.
 	 *
-	 * @throws IOException Unexpected.
+	 * @throws IOException
+	 *             Unexpected.
 	 */
 	@Test
 	public void testIsDirectory_NotDirectory() throws IOException {
-		final Path path = Files.createTempFile(PREFIX, null, OWNER_READ_ATTRIBUTE);
-		path.toFile().deleteOnExit();
-
-		assertFalse(validator.isDirectory(path));
+		final Path path = createTemporaryFile(true);
+		assertFalse("Path is directory: " + path.toString(), validator.isDirectory(path));
 	}
 
 	/**
-	 * Tests that {@link FileValidator#isDirectory(Path)} returns {@code true}
-	 * when the given {@link Path} refers to a file which is a directory.
+	 * Tests that {@link FileValidator#isDirectory(Path)} returns {@code true} when
+	 * the given {@link Path} refers to a file which is a directory.
 	 *
-	 * @throws IOException Unexpected.
+	 * @throws IOException
+	 *             Unexpected.
 	 */
 	@Test
 	public void testIsDirectory_Directory() throws IOException {
-		final Path path = Files.createTempDirectory(PREFIX, OWNER_READ_ATTRIBUTE);
-		path.toFile().deleteOnExit();
-
-		assertTrue(validator.isDirectory(path));
+		final Path path = createTemporaryDirectory();
+		assertTrue("Path is not directory: " + path.toString(), validator.isDirectory(path));
 	}
 
 	/**
-	 * Tests that {@link FileValidator#isExecutable(Path)} throws an exception
-	 * when the given {@link Path} is {@code null}.
+	 * Tests that {@link FileValidator#isExecutable(Path)} throws an exception when
+	 * the given {@link Path} is {@code null}.
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testIsExecutable_NullPath() {
@@ -91,28 +79,42 @@ public class FileValidatorTest {
 	 * Tests that {@link FileValidator#isExecutable(Path)} returns {@code false}
 	 * when the given {@link Path} refers to a file which is not executable.
 	 *
-	 * @throws IOException Unexpected.
+	 * @throws IOException
+	 *             Unexpected.
 	 */
 	@Test
 	public void testIsExecutable_NotExecutable() throws IOException {
-		final Path path = Files.createTempFile(PREFIX, null, OWNER_READ_ATTRIBUTE);
-		path.toFile().deleteOnExit();
-
-		assertFalse(validator.isExecutable(path));
+		final Path path = createTemporaryFile(false);
+		assertFalse("Path is executable: " + path.toString(), validator.isExecutable(path));
 	}
 
 	/**
-	 * Tests that {@link FileValidator#isExecutable(Path)} returns {@code true}
-	 * when the given {@link Path} refers to a file which is executable.
+	 * Tests that {@link FileValidator#isExecutable(Path)} returns {@code true} when
+	 * the given {@link Path} refers to a file which is executable.
 	 *
-	 * @throws IOException Unexpected.
+	 * @throws IOException
+	 *             Unexpected.
 	 */
 	@Test
 	public void testIsExecutable_Executable() throws IOException {
-		final Path path = Files.createTempFile(PREFIX, null, OWNER_EXECUTE_ATTRIBUTE);
-		path.toFile().deleteOnExit();
+		final Path path = createTemporaryFile(true);
+		assertTrue("Path is not executable: " + path.toString(), validator.isExecutable(path));
+	}
 
-		assertTrue(validator.isExecutable(path));
+	private static Path createTemporaryFile(final boolean isExecutable) throws IOException {
+		final File file = Files.createTempFile("hydrogen-junit", ".tmp").toFile();
+		file.setExecutable(isExecutable);
+		if (!isExecutable) {
+			file.delete(); // Delete now to make it non-executable
+		}
+		file.deleteOnExit();
+		return file.toPath();
+	}
+
+	private static Path createTemporaryDirectory() throws IOException {
+		final File file = Files.createTempDirectory("hydrogen-junit").toFile();
+		file.deleteOnExit();
+		return file.toPath();
 	}
 
 }
